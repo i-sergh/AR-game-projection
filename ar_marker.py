3dc9386b23e647aa291fd88d71784a527eb41444
i-sergh <seripp28@gmail.com>
2023-04-22 15:23:45 +0300

    'game'

--- ar_marker.py
+++ ar_marker.py
@@ -2,20 +2,43 @@ import cv2
 import numpy as np
 from markerFinder import findOneContour, findCenter, drawReferencePoint, getReferencePoint, buildSquare
 
-from game.game import game, play
+from game.game import game, play, CONTROLLER
 from image_deformer import place_image
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
+
+cap1 = cv2.VideoCapture(0)
+
 
 while True:
     tr, frame = cap.read()
+    tr, frame_controller = cap1.read()
+    
     frame = cv2.flip(frame, 2)
     frame_ = cv2.blur( frame, (40, 40) )
     frame_hsv = cv2.cvtColor(frame_, cv2.COLOR_BGR2HSV)
+
+    frame_controller = cv2.flip(frame_controller, 2)
+    frame_ = cv2.blur( frame_controller, (40, 40) )
+    frame_hsv_controller = cv2.cvtColor(frame_, cv2.COLOR_BGR2HSV)
+    
     dots = []
+
+    # red_controller
+    
+    clr_low = (0,120,120)
+    clr_high = (15,255,255)
+    drawReferencePoint(frame_hsv_controller, frame_controller, clr_low, clr_high)
+    red_dot = getReferencePoint(frame_hsv_controller, frame_controller, clr_low, clr_high)
+    if red_dot :
+        cv2.putText(frame_controller, 'YOU', red_dot , cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (255,0,0), 2, cv2.LINE_AA)
+        CONTROLLER.CONTROLLER = red_dot[1]
+        
     # red
-    clr_low = (0,90,20)
+    
+    clr_low = (0,120,120)
     clr_high = (15,255,255)
     drawReferencePoint(frame_hsv, frame, clr_low, clr_high)
     red_dot = getReferencePoint(frame_hsv, frame, clr_low, clr_high)
@@ -57,10 +80,13 @@ while True:
     buildSquare(frame, dots)
 
     play()
-    place_image(frame, game,  dots)
-        
+    try:    
+        place_image(frame, game,  dots)
+    except:
+        pass
+    
     cv2.imshow('', frame)
-
+    cv2.imshow('controller', frame_controller)
     key = cv2.waitKey(1)
     
     if key == 27:
--- game/game.py
+++ game/game.py
@@ -6,21 +6,64 @@ from time import time
 game = np.zeros((700, 500, 3), dtype= np.uint8() )
 dmg_field = np.zeros( (game.shape[0], game.shape[1]), dtype = np.uint8())
 
+class Controller:
+    CONTROLLER = 430
+
 class Player:
+    x_pos = 250
+    y_pos = 650
     
-    
+    size_x = 100
+    size_y = 30
+
+    x = int(x_pos - size_x/2)
+    y = int(y_pos - size_y/2)
+
+    speed = 50
+
+    shoot_delay = 1
+    SHOOT_DELAY_MAX = shoot_delay
+    color = (150, 250, 0)
+        
     def draw(self):
-        cv2.rectangle(game, (self.x,self.y), (self.x+ self.size, self.y+self.size), self.color, -1)
+        cv2.rectangle(game, (self.x,self.y), (self.x+ self.size_x, self.y+self.size_y), self.color, -1)
         
     def destroy(self):
-        cv2.rectangle(game, (self.x,self.y), (self.x+ self.size, self.y+self.size), (0,0,0), -1)
-    
+        cv2.rectangle(game, (self.x,self.y), (self.x+ self.size_x, self.y+self.size_y), (0,0,0), -1)
 
+    def move(self):
+        self.destroy()
+        
+        if CONTROLLER.CONTROLLER > self.x_pos:
+            self.move_right()
+        elif CONTROLLER.CONTROLLER < self.x_pos:
+            self.move_left()
+
+        if self.shoot_delay == self.SHOOT_DELAY_MAX:
+            self.shoot()
+            self.shoot_delay = 0
+        else:
+            self.shoot_delay += 1
+        
+        self.draw()
+
+    
+    def move_left(self):
+        self.x -= self.speed
+        self.x_pos -= self.speed
+
+    def move_right(self):
+        self.x += self.speed
+        self.x_pos += self.speed
+
+    def shoot(self):
+        bs.append(Bullet(self.x_pos, self.y_pos) )
+        
 class Particle:
     def __init__(self, x, y):
         self.x = x + randint(-20, 20)
         self.y = y + randint(-20, 20)
-        self.size = randint(20, 50)
+        self.size = randint(10, 30)
 
         self.live_tick = 25
         
@@ -31,11 +74,12 @@ class Particle:
         self.speedY = randint(-5, 5)
     
     def draw(self):
-        cv2.rectangle(game, (self.x,self.y), (self.x+ self.size, self.y+self.size), self.color, -1)
-        
+        #cv2.rectangle(game, (self.x,self.y), (self.x+ self.size, self.y+self.size), self.color, -1)
+        cv2.circle( game, (self.x,self.y), self.size, self.color, -1 )
     def destroy(self):
-        cv2.rectangle(game, (self.x,self.y), (self.x+ self.size, self.y+self.size), (0,0,0), -1)
-
+        #cv2.rectangle(game, (self.x,self.y), (self.x+ self.size, self.y+self.size), (0,0,0), -1)
+        cv2.circle( game, (self.x,self.y), self.size, (0,0,0), -1 )
+        
     def move(self):
         self.destroy()
         
@@ -49,28 +93,35 @@ class Particle:
         
         
 class Rect:
-    x = 100
+    x = randint(50, 400)
     y = 10
     size = 50
     hp = 100
     speed = 100
     nockback = 5
     last_hit_time = time()
+    start_delay = randint(0, 10)
+    
     def draw(self):
-        G = int(self.hp * 2.54)
-        R = int(100 - self.hp * 2.54)
-        cv2.rectangle(game, (self.x,self.y), (self.x+ self.size, self.y+self.size), (1,G+1,R+1), -1)
+        G = int(self.hp * 2.50)
+        R = int(100 - self.hp * 2.50)
+        cv2.rectangle(game, (self.x,self.y), (self.x+ self.size, self.y+self.size), (20,G+1,R+1), -1)
         
     def destroy(self):
         cv2.rectangle(game, (self.x,self.y), (self.x+ self.size, self.y+self.size), (0,0,0), -1)
         
     def move(self):
+        if self.start_delay > 0:
+            self.start_delay -= 1
+            return None
         self.destroy()
         self.damage_check()
         
         self.y +=self.speed
         if self.speed != 150:
             self.speed+=1
+
+
         self.draw()
         
         if self.y > game.shape[0]:
@@ -79,13 +130,14 @@ class Rect:
     def respawn(self):
         for _ in range(randint(3, 7)):
             particles.append( Particle( int(self.x +self.size/2), int(self.y +self.size/2) ) )
-        self.x = randint(100, 150)
+        self.x = randint(50, 400)
         self.y = 10
         self.size = 50
         self.hp = 100
         self.speed = 1
         self.nockback = 5
         self.last_hit_time = time()
+        self.start_delay = randint(0, 10)
         
     def damage_check(self):
         if (dmg_field[self.y:self.y+self.size,  self.x:self.x+ self.size] > 0).any() and time() - self.last_hit_time > 0.1:
@@ -98,18 +150,21 @@ class Rect:
                 
 class Bullet:
     
-    y = 700
+    #y = 700
     sizeX = 10
     sizeY = 50
-    dmg = 10
+    dmg = randint(10, 20)
     speed = 50
-    def __init__(self):
-        self.start_delay = randint( 0, 100)
-        self.x = randint(50, 200)
+    dead = False
+    
+    def __init__(self, x, y  ):
+        self.start_delay = 0# randint( 0, 10)
+        self.x = x + randint(-50, 50)
+        self.y = y - self.sizeY
 
         
     def draw(self):
-        cv2.rectangle(game, (self.x,self.y), (self.x+ self.sizeX, self.y+self.sizeY), (0,255,255), -1)
+        cv2.rectangle(game, (self.x,self.y), (self.x+ self.sizeX, self.y+self.sizeY), (20,250,250), -1)
         dmg_field[self.y:self.y+self.sizeY,  self.x:self.x+ self.sizeX] += self.dmg
 
     def destroy(self):
@@ -126,24 +181,33 @@ class Bullet:
 
         if self.y < 0-self.sizeY:
             self.destroy()
-            self.y = 700
-            self.x = randint(50, 200)
-            self.dmg = randint(10, 20)
+            self.dead = True
+            #self.y = 700
+            #self.x = randint(50, 200)
+            #self.dmg = randint(10, 20)
 
+CONTROLLER = Controller()
 particles = []
-r = Rect()
-bs = [ Bullet() for _ in range(5)]
+rs = [ Rect() for _ in range(5)]
+bs = [] #[ Bullet() for _ in range(5)]
+player = Player()
+
 
 def play():
     print(' '* 100,  end='\r')
-    r.move()
-    for b in bs:
+    for r in rs:
+        r.move()
+    for ind, b in enumerate(bs):
         b.move()
+        if b.dead:
+            bs.pop(ind)
     for ind, p in enumerate(particles):
         p.move()   
         if p.live_tick <= 0:
             particles.pop(ind)
-    print('hp: ', r.hp, '; p length ', len(particles),  end='\r')
+    print('hp: ', r.hp, ' bullets: ', len(bs),  end='\r')
+
+    player.move()
 
 if __name__ == '__main__':
     r = Rect()
