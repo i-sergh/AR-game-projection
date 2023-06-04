656faf1551a939cd4fe73dcd85da211f9fb9c0ea
i-sergh <seripp28@gmail.com>
2023-06-04 20:41:18 +0300

    color setings"

--- ar_marker.py
+++ ar_marker.py
@@ -6,6 +6,13 @@ from game.game import game, play, CONTROLLER
 from image_deformer import place_image
 
 
+def dots_from_sequence(sequence, dots_dict):
+    dots = []
+    for element in sequence:
+        if dots_dict[element]:
+            dots.append(dots_dict[element])
+    return dots
+
 cap = cv2.VideoCapture(1)
 
 cap1 = cv2.VideoCapture(0)
@@ -23,8 +30,9 @@ while True:
     frame_ = cv2.blur( frame_controller, (40, 40) )
     frame_hsv_controller = cv2.cvtColor(frame_, cv2.COLOR_BGR2HSV)
     
-    dots = []
-
+    ## ! проверить
+    #dots = []
+    dots_dict = {'red': None, 'blue': None, 'yellow': None, 'green': None}
     # red_controller
     
     clr_low = (0,120,120)
@@ -45,7 +53,8 @@ while True:
     if red_dot :
         cv2.putText(frame, 'DOT 1', red_dot , cv2.FONT_HERSHEY_SIMPLEX, 
                    1, (255,0,0), 2, cv2.LINE_AA)
-        dots.append(red_dot)
+        dots_dict['red'] = red_dot
+        #dots.append(red_dot)
 
 
     # blue
@@ -56,7 +65,8 @@ while True:
     if blue_dot :
         cv2.putText(frame, 'DOT 2', blue_dot , cv2.FONT_HERSHEY_SIMPLEX, 
                    1, (255,0,0), 2, cv2.LINE_AA)
-        dots.append(blue_dot)
+        dots_dict['blue'] = blue_dot
+        #dots.append(blue_dot)
 
     # yellow
     clr_low = (15,110,90)
@@ -66,7 +76,8 @@ while True:
     if yellow_dot :
         cv2.putText(frame, 'DOT 3', yellow_dot , cv2.FONT_HERSHEY_SIMPLEX, 
                    1, (255,0,0), 2, cv2.LINE_AA)
-        dots.append(yellow_dot)
+        dots_dict['yellow'] = yellow_dot
+        #dots.append(yellow_dot)
 
     # green
     clr_low = (35,50,90)
@@ -76,9 +87,12 @@ while True:
     if green_dot :
         cv2.putText(frame, 'DOT 4', green_dot , cv2.FONT_HERSHEY_SIMPLEX, 
                    1, (255,0,0), 2, cv2.LINE_AA)
-        dots.append(green_dot)
+        dots_dict['green'] = green_dot
+        #dots.append(green_dot)
 
-    
+    sequence = ['red', 'blue', 'yellow', 'green']
+    dots = dots_from_sequence(sequence, dots_dict)
+
     buildSquare(frame, dots)
 
     play()
